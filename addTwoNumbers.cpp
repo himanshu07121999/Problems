80e4a74022c8b7f67b9cc5a1154990109200cce3
Himanshu <57253980+himanshu07121999@users.noreply.github.com>
2021-12-27 13:53:58 +0530

    Create addTwoNumbers.cpp

---
+++ addTwoNumbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    std::cin >> t;
+    int t1,t2;
+    int t3;
+    for (int i=0;i<t;i++) {
+        cin>>t1>>t2;
+         t3=t1+t2;
+         std::cout << t3 << std::endl;
+         
+    }
+}
